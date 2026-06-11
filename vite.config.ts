ee3548295e20028129e20499252583523118f010
Adam Polický <adam.policky@gmail.com>
2026-06-11 19:47:16 +0200

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/backstage/',
+  base: '/',
   plugins: [react(), tailwindcss()],
 })
